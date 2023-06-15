ab8e147f1fc6bdcce2c3fb12b56cadeecfae6049
samol <samol@DESKTOP-M1TF6E9>
2023-06-15 13:51:56 +0300

    Add project files.

---
+++ Program(server-client)/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Program_server_client_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UdpClient udpClient = new UdpClient();
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.1.106"), 3344);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PushButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(type.Text != null)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(type.Text);
+                udpClient.Send(data, data.Length, endPoint);
+                IPEndPoint serverEndPoint = null;
+                byte[] response = udpClient.Receive(ref serverEndPoint);
+                string responseMessage = Encoding.UTF8.GetString(response);
+                responseText.Text += responseMessage + " " + DateTime.Now.ToShortTimeString() + "\n";
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new()
+            {
+                ["Ukraine"] = "Kyiv",
+                ["Poland"] = "Warsaw",
+                ["Germany"] = "Berlin",
+                ["Belgium"] = "Brussels",
+                ["Brazil"] = "Brasília",
+                ["Bulgaria"] = "Sofia",
+                ["Cambodia"] = "Phnom Penh",
+                ["Canada"] = "Ottawa",
+                ["China"] = "Beijing",
+                ["Colombia"] = "Bogotá",
+                ["Cuba"] = "Havana",
+                ["Dominica"] = "Roseau",
+                ["Egypt"] = "Cairo"
+            };
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.1.106"), 3344);
+
+            UdpClient server = new UdpClient(endPoint);
+
+            while (true)
+            {
+                Console.WriteLine("...Waiting for the request...");
+
+                IPEndPoint clientEndPoint = null;
+
+                byte[] request = server.Receive(ref clientEndPoint);
+
+                string message = Encoding.UTF8.GetString(request);
+                Console.WriteLine($"Received message: {message} : {DateTime.Now.ToShortTimeString()} from {clientEndPoint}");
+                string responses = " ";
+                if (dictionary.ContainsKey(message))
+                    responses = dictionary[message];
+                else
+                    responses = "I can't find this country";
+                byte[] response = Encoding.UTF8.GetBytes(responses);
+                server.Send(response, response.Length, clientEndPoint);
+            }
+        }
+    }
+}
